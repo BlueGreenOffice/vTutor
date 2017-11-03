1a0ffb5d979f0b9d95eb5c0a3d6f8ad4ad3603b3
cummingsi1993 <cummingsi1993@gmail.com>
2017-11-02 22:02:07 -0400

    initial checkin

---
+++ VTutor.Web/Controllers/SubjectsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VTutor.Model;
+using VTutor.Web.Models;
+
+namespace VTutor.Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Subjects")]
+    public class SubjectsController : Controller
+    {
+        private readonly VTutorWebContext _context;
+
+        public SubjectsController(VTutorWebContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Subjects
+        [HttpGet]
+        public IEnumerable<Subject> GetSubject()
+        {
+            return _context.Subject;
+        }
+
+        // GET: api/Subjects/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSubject([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var subject = await _context.Subject.SingleOrDefaultAsync(m => m.id == id);
+
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(subject);
+        }
+
+        // PUT: api/Subjects/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSubject([FromRoute] Guid id, [FromBody] Subject subject)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != subject.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(subject).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SubjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Subjects
+        [HttpPost]
+        public async Task<IActionResult> PostSubject([FromBody] Subject subject)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Subject.Add(subject);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSubject", new { id = subject.id }, subject);
+        }
+
+        // DELETE: api/Subjects/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSubject([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var subject = await _context.Subject.SingleOrDefaultAsync(m => m.id == id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            _context.Subject.Remove(subject);
+            await _context.SaveChangesAsync();
+
+            return Ok(subject);
+        }
+
+        private bool SubjectExists(Guid id)
+        {
+            return _context.Subject.Any(e => e.id == id);
+        }
+    }
+}
