c9d1eb7f7397d51af7cf9a550347db08571105c1
Isaac Cummings <Cummingsi1993@gmail.com>
2018-02-22 20:04:28 -0500

    savem

---
+++ VTutor.Web/Server/Controllers/EmailController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VTutor.Model;
+using Microsoft.AspNetCore.Authorization;
+
+namespace VTutor.Web.Server.Controllers
+{
+	[Produces("application/json")]
+	[Route("api/Emails")]
+	public class EmailController : Controller
+	{
+
+		public EmailController()
+		{
+
+		}
+
+		[HttpPost]
+		[Route("tutor-interest")]
+		public async Task<IActionResult> TutorContactForm(Email.TemplateModels.TutorContactForm form)
+		{
+			Email.EmailClient.SendTutorInterestEmail(form);
+			return Ok();
+		}
+
+
+
+    }
+}
